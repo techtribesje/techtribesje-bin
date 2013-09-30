086138f3a535769165deaa1d10407933eafc934f
Simon Brown <simon.brown@codingthearchitecture.com>
2013-09-30 15:53:35 +0100

    tar'd data backups.

--- backup-data.sh
+++ backup-data.sh
@@ -6,3 +6,7 @@ mongoexport --db techtribesje --collection newsFeedEntries --out $DATA/$TODAY-mo
 mongoexport --db techtribesje --collection tweets --out $DATA/$TODAY-mongodb-tweets.json
 
 mysqldump --lock-tables techtribesje > $DATA/$TODAY-mysql-techtribesje.sql
+
+tar cjf $DATA/$TODAY.tar.bz2 -C $DATA $TODAY-mongodb-newsFeedEntries.json $TODAY-mongodb-tweets.json $TODAY-mysql-techtribesje.sql
+rm $DATA/$TODAY*.json
+rm $DATA/$TODAY*.sql
